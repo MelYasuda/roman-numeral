3909c2b1494a9b1066cfedd50b0c9fa9a8f90627
Meria Thomas and Mel Yasuda <mel@>
2018-08-16 11:04:26 -0700

    change if to while loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,42 @@
 // Business logic
-var numberCheck = function(input){
-  if (!input.match(/^[0-9]+$/)){
-    alert("Please enter a valid number.")
-  }
-  else if( input > 3999){
-    alert("Enter a number less than 4000!");
-  }
-}
+// var numberCheck = function(input){
+//   if (!input.match(/^[0-9]+$/)){
+//     alert("Please enter a valid number.")
+//   }
+//   else if( input > 3999){
+//     alert("Enter a number less than 4000!");
+//   }
+//   else{
+//     input = parseInt(input);
+//   }
+// }
 var operation = function(input) {
   var decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
   var roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
-  input = parseInt(input);
   var result ="";
 
   for(var i=0; i<decimal.length; i++){
-    if(input%decimal[i] < input){
+    while(input%decimal[i] < input){
       result = result + roman[i];
-      alert(result);
-      input = input%decimal[i];
+      input = input-decimal[i];
     }
     }
     return result;
 
   }
 
-
-
 // user interface
 $(document).ready(function(){
   $("#input").submit(function(event){
     event.preventDefault();
 
-    var input = $("#number").val();
+    var input = parseInt($("#number").val());
+    if(input>3999){
+      alert("Enter a number less than 4000");
+    }
+    else{
+      $("#result").append(operation(input));
+    }
 
-    $("#result").append(numberCheck(operation(input)));
   });
 });
